225979dbf540c1256a5506166c4f2e1e68eb90ce
KosumosuL <hubukizhao@gmail.com>
2019-03-23 12:29:03 +0800

    basic template

--- BookManage/config.py
+++ BookManage/config.py
@@ -1,6 +1,11 @@
+from datetime import timedelta
+
 DEBUG = True
 PORT = 5000
 HOST = "localhost"
+
+SECRET_KEY = "bookmanage"
+PERMANENT_SESSION_LIFETIME = timedelta(days=7)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
--- BookManage/view.py
+++ BookManage/view.py
@@ -1,14 +1,24 @@
-from flask import jsonify, request, render_template, redirect
+from flask import jsonify, request, render_template, redirect, session, url_for
 from BookManage.model import *
 from werkzeug.security import safe_str_cmp
 from collections import OrderedDict
 from datetime import datetime
+
 
 def init_api(app):
 
     @app.route('/')
     def index():
         return render_template('index.html')
+
+    @app.route('/login', methods=['POST'])
+    def login():
+        pass
+
+    @app.route('/register', methods=['POST'])
+    def register():
+        pass
+
 
     @app.route('/manager_login', methods=['GET', 'POST'])
     def manager_login():
@@ -22,3 +32,7 @@ def init_api(app):
                 session['user_id'] = app.config['MANAGER_NAME']
                 return redirect(url_for('manager'))
         return render_template('manager_login.html', error=error)
+
+    @app.route('/search', methods=['POST'])
+    def search():
+        pass
